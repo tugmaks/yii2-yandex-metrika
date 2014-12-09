48a6227c0bd4633d60eb83c6ab607c7e17ba0aba
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-09 14:37:25 +0400

    roles

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -2,12 +2,28 @@
 
 namespace tugmaks\YandexMetrika\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\filters\AccessControl;
 
-class DefaultController extends Controller
-{
-    public function actionIndex()
-    {
+class DefaultController extends Controller {
+
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => Yii::$app->controller->module->allowedRoles,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex() {
         return $this->render('index');
     }
+
 }
