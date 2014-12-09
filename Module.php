fb67d057dd2f7dfb35ea639d4f56c82e70ce7db4
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-09 15:28:14 +0400

    curl wrapper

--- Module.php
+++ Module.php
@@ -6,6 +6,8 @@ class Module extends \yii\base\Module {
 
     public $allowedRoles = ['Superadministrator'];
     public $controllerNamespace = 'tugmaks\YandexMetrika\controllers';
+    public $OAuthToken;
+    public $OAuthPassword;
 
     public function init() {
         parent::init();
