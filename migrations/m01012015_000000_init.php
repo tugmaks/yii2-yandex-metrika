a8fda785b2c0cd1dcdfd3914c0276fddebfe516e
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-09 18:06:07 +0400

    yandex metrika

--- migrations/m01012015_000000_init.php
+++ migrations/m01012015_000000_init.php
@@ -3,7 +3,7 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class init extends Migration {
+class m01012015_000000_init extends Migration {
 
     public function safeUp() {
         $this->createTable('tbl_ym_settings', [
