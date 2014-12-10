11f6d0cc4fb62cc70572b4597614178c7e4846a3
tugmaks <tugmaks@home>
2014-12-10 23:47:29 +0400

    h

--- views/default/_counter.php
+++ views/default/_counter.php
@@ -6,6 +6,7 @@ use tugmaks\YandexMetrika\Module;
 <div style="margin: 5px;width: 250px; float: left">
 
     <?php
+    var_dump($model);
     //echo
 //    DetailView::widget([
 //        'model' => $model,
