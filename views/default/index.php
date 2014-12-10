8a8af8b8a206c9f7a3f5656e014fa13766797fc7
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-10 16:24:59 +0400

    small fix

--- views/default/index.php
+++ views/default/index.php
@@ -4,6 +4,7 @@ use yii\widgets\ListView;
 
 echo ListView::widget(['dataProvider' => $provider, 'itemView' => '_counter', 'layout' => "{items} \n {pager}"]);
 ?>
-<?php var_dump($this->context->module->getCounters()) ?>
+----------------------
+<?php var_dump($this->context->module->getCounters()); ?>
 
 
