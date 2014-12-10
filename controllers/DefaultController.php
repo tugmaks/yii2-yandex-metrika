659ceb05de5569f6fecec019fab56225a7d7b572
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-10 14:16:42 +0400

    small fix

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -37,7 +37,7 @@ class DefaultController extends Controller {
         $provider = new ArrayDataProvider([
             'allModels' => $counters,
         ]);
-        return $this->render('index', ['provider' => $provider,'counters'=>  $this->module->callApi('counters')]);
+        return $this->render('index', ['provider' => $provider,'counters'=>  $this->module->callApi('counters')->counters]);
     }
 
     public function actionVerificationCode() {
