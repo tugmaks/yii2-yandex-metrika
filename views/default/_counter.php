b908008b623b877cdb5c817918f1220d63223ca4
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-10 17:18:39 +0400

    small fix

--- views/default/_counter.php
+++ views/default/_counter.php
@@ -3,5 +3,15 @@
 use yii\widgets\DetailView;
 ?>
 <div style="margin: 5px;width: 250px; float: left">
-    <?= DetailView::widget(['model' => $model]); ?>
+    <?=
+    DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            [
+                'label' => 'Статус',
+                'value' => $model->code_status,
+            ],
+        ],
+    ]);
+    ?>
 </div>
