6529786f58272bbc74acf3071d9975fcecb62bc6
tugmaks <tugmaks@home>
2014-12-10 23:47:07 +0400

    h

--- views/default/_counter.php
+++ views/default/_counter.php
@@ -4,16 +4,17 @@ use yii\widgets\DetailView;
 use tugmaks\YandexMetrika\Module;
 ?>
 <div style="margin: 5px;width: 250px; float: left">
-    
-    <?=
-    DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'label' => 'Статус',
-                'value' => $this->context->module->getCounterStatus($model->code_status),
-            ],
-        ],
-    ]);
+
+    <?php
+    //echo
+//    DetailView::widget([
+//        'model' => $model,
+//        'attributes' => [
+//            [
+//                'label' => 'Статус',
+//                'value' => $this->context->module->getCounterStatus($model->code_status),
+//            ],
+//        ],
+//    ]);
     ?>
 </div>
